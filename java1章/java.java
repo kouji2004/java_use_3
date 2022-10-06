c6d5dee602d68c2e93690c4004b3b57df9c7478c
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-06 19:03:10 +0900

    基本的な文字列操作

--- java1章/java.java
+++ java1章/java.java
@@ -1,16 +1,22 @@
 package java1章;
 
 public class java {
-  public static void main(String[]ages){
+  public static void main(String[] ages) {
     String s1 = "すっきりjava";
     String s2 = "java";
     String s3 = "jAva";
 
-    if(s3.equals(s2)){
+    if (s3.equals(s2)) {
       System.out.println("一致");
     }
-    if(s2.equalsIgnoreCase(s3)){
+    if (s2.equalsIgnoreCase(s3)) {
       System.out.println("s2とs3はケースを区別しなければ等しい");
     }
+    System.out.println("s1の長さは" + s1.length() + "です");
   }
 }
+
+// 内容が等しいか調べる ＝ equals
+// 大文字小文字を区別せず内容が等しいか調べる ＝ equalsIgnoreCase
+// 文字列長を調べる = length()
+// 空文字を調べる = isEmpty()
