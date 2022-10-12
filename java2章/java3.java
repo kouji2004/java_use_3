30f88c602ae216d047895278ac0d96e4a3ee06bb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-12 16:41:19 +0900

    メソッドの呼び出し

---
+++ java2章/java3.java
@@ -0,0 +1,13 @@
+package java2章;
+
+public class java3 {
+  public static void main(String[] ages) {
+    System.out.println("メソッドの呼び出し");
+    hello();
+    System.out.println("メソッドの終わり");
+  }
+
+  public static void hello() {
+    System.out.println("こんにちは");
+  }
+}
