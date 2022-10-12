fcc1de152e0edc6ff2d151e5558a4ed0ef5afb61
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-12 16:48:47 +0900

    mainメソッド以外からメソッドを呼び出す

---
+++ java2章/java3_3.java
@@ -0,0 +1,24 @@
+package java2章;
+
+public class java3_3 {
+  public static void methodA() {
+    System.out.println("AmethodA");
+    methodB();
+  }
+
+  public static void methodB() {
+    System.out.println("methodB");
+  }
+
+  // public static void methodC() {
+  // System.out.println("methodC");
+  // }
+
+  public static void main(String[] ages) {
+    methodA();
+  }
+}
+
+// 実行結果
+// Amethod
+// methodB
