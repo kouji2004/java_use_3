29edd4b4d5162c56c38fcb064e7a1eedc236d852
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-13 19:51:24 +0900

    同じ配列を参照していることを確認

--- java2章/java4_6.java
+++ java2章/java4_6.java
@@ -1,7 +1,18 @@
 package java2章;
 
 public class java4_6 {
+
   public static void main(String[]ages){
-System.out.println("a");
+    int[] array = {1,2,2};
+    incArray(array);
+    for(int i:array){
+      System.out.println(i);
+    }
+  }
+  public static void incArray(int[] array){
+    for(int i = 0; i<array.length; i++){
+      array[i]++;
+    }
 }
+
 }
