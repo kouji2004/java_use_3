8f9c61e7a13017f9696fa74e335bb257dc9129eb
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-13 21:25:25 +0900

    複数クラスを用いた開発”

---
+++ java３章/Calc.java
@@ -0,0 +1,10 @@
+package java３章;
+
+public class Calc{
+  public static void main(String[]ages){
+    int a = 10; int b = 2;
+    int total = Logic.tasu(a,b);
+    int delta = Logic.hiku(a,b);
+    System.out.println("足すと"+total+"引くと"+delta);
+    }
+}
---
+++ java３章/Logic.java
@@ -0,0 +1,10 @@
+package java３章;
+
+public class Logic{
+  public static int tasu(int a, int b){
+    return (a + b);
+  }
+  public static int hiku(int a, int b){
+    return (a - b);
+  }
+}
