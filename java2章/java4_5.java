f50599b5fbcf9e51b6efc7ec5fcfc6800ee1e9c9
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-13 19:43:09 +0900

    配列が引数

--- java2章/java4_5.java
+++ java2章/java4_5.java
@@ -1,16 +1,16 @@
-package java2章;
+// package java2章;
 
-public class java4_5 {
-  public static void addArray(int[]array){
-    for(int eleme : array){
-      System.out.println(eleme);
-    }
-  }
-  public static void main(String[]ages){
-    int[] array = {1,2,3};
-     addArray(array);
-  }
-}
+// public class java4_5 {
+//   // public static void addArray(int[]array){
+//   //   for(int element:array){
+//   //     System.out.println(element);
+//   //   }
+//   // }
+//   // public static void main(String[]ages){
+//   //   int[] array = {1,2,3};
+//   //    addArray(array);
+//   // }
+// }
 
 //実行結果
 //1
---
+++ java2章/java4_6.java
@@ -0,0 +1,7 @@
+package java2章;
+
+public class java4_6 {
+  public static void main(String[]ages){
+System.out.println("a");
+}
+}
