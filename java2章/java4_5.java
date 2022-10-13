f22769d936b2446ac2ef87df3db0817cddc668aa
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-10-13 19:35:09 +0900

    配列が引数

---
+++ java2章/java4_5.java
@@ -0,0 +1,13 @@
+package java2章;
+
+public class java4_5 {
+  public static void addArray(int[]array){
+    for(int eleme : array){
+      System.out.println(eleme);
+    }
+  }
+  public static void main(String[]ages){
+    int[] array = {1,2,3};
+     addArray(array);
+  }
+}
